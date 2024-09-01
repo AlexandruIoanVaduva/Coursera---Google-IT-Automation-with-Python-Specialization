fdad6e53e328a01163c0a98b8c52e4f378b63102
AlexandruIoanVaduva <144168443+AlexandruIoanVaduva@users.noreply.github.com>
2024-09-01 14:55:31 +0300

    Create run.py

---
+++ Module 4.Final project/run.py
@@ -0,0 +1,23 @@
+# third task for the final project
+#! /usr/bin/env python3
+import os
+import requests
+
+file_list = [f for f in os.listdir("supplier-data/descriptions") if ".txt" in f]
+
+dict = {"name":[],"weight":[],"description":[],"image_name":[]}
+for y in file_list:
+    with open("supplier-data/descriptions/" + y) as x:
+        lines = [line.rstrip('\n') for line in x]
+    dict["name"] = lines[0]
+    weight = lines[1]
+    dict["weight"] = int(weight[:-4])
+    dict["description"] = lines[2]
+    dict["image_name"] = y[:-3] + "jpeg"
+    try:
+        response = requests.post("http://link/fruits/", data=dict)
+        response.request.url
+        response.request.body
+        print(response.status_code)
+    except:
+        print("error")
